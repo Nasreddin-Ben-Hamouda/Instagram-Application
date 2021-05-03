583edd909a172b7c580a78b1244f3ee4b8226f61
Nasr Eddine <nasreddinbenhamouda@gmail.com>
2021-05-03 08:11:35 +0100

    display posts with likes,comments & add,delete comment ...

--- app/src/main/java/com/example/instagram/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagram/Fragments/ProfileFragment.java
@@ -14,7 +14,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
